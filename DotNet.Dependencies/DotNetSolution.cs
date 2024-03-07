04b67d4fe5073d28585b3bf15195d3462c5e44c1
Adam O'Neil <adamosoftware@gmail.com>
2024-03-07 07:53:32 -0500

    started on VS inspection

---
+++ DotNet.Dependencies/DotNetSolution.cs
@@ -0,0 +1,34 @@
+﻿using Buildalyzer;
+using Microsoft.Build.Construction;
+
+namespace DotNet.Dependencies;
+
+public class Project
+{
+    public string Name { get; set; } = default!;
+    public string Path { get; set; } = default!;
+    public string[] ProjectReferences { get; set; } = [];
+    public string[] PackageReferences { get; set; } = [];
+}
+
+public static class DotNetSolution
+{
+    public static IEnumerable<Project> Analyze(string solutionFile)
+    {
+        var manager = new AnalyzerManager();
+        
+        var solution = SolutionFile.Parse(solutionFile);
+        foreach (var project in solution.ProjectsInOrder) 
+        {
+            var analyzer = manager.GetProject(project.AbsolutePath);
+
+            throw new NotImplementedException("add dependencies to returned item");
+
+            yield return new Project()
+            {
+                Name = project.ProjectName,
+                Path = project.AbsolutePath                
+            };
+        }        
+    }    
+}
